c1d04c6fc043a359150daa8895b3272f21aca7a9
alonso fabila <jorgealonsofabila@gmail.com>
2023-11-03 17:59:37 -0600

    Secrets added

--- src/azure-image-analysis.js
+++ src/azure-image-analysis.js
@@ -1,8 +1,8 @@
 const key = process.env.AZURE_SECRET_VISION_KEY;
 const endpoint = process.env.AZURE_SECRET_VISION_ENDPOINT;
 
-console.log(key);
-console.log(endpoint);
+console.log(`key = ${key}`)
+console.log(`endpoint = ${endpoint}`)
 
 // Cognitive Services API 
 const visualFeatures = [
@@ -17,28 +17,33 @@ const visualFeatures = [
 
 
 const analyzeImage = async (imageUrl) => {
-    const requestUrl = `https://azure-image-analysis.cognitiveservices.azure.com/computervision/imageanalysis:analyze&api-version=2023-02-01-preview&features=${visualFeatures.join(',')}`;
-    console.log(`requesed Url = ${requestUrl}`);
+    const requestUrl = `${endpoint}computervision/imageanalysis:analyze?api-version=2023-02-01-preview&features=${visualFeatures.join(',')}`;
+    console.log(`requestUrl = ${requestUrl}`)
 
 
-    //Analyze the image from imageURL
-    const response = await fetch(requestUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Ocp-Apim-Subscription-Key': key
-        },
-        body: JSON.stringify({url: imageUrl}),
-        features: visualFeatures.join(',')
-    });
+    try {
+        //Analyze the image from imageURL
+        const response = await fetch(requestUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Ocp-Apim-Subscription-Key': key
+            },
+            body: JSON.stringify({url: imageUrl}),
+            features: visualFeatures.join(',')
+        });
 
-    if(!response.ok) {
-        throw new Error(`An error has occured: ${response.status}`);
+        if (!response.ok) {
+            throw new Error(`Computer Vision request failed: ${response.status} (${response.statusText})`);
+        }
+
+        const analysis = await response.json();
+
+        return { "URL": urlToAnalyze, ...analysis};
+    } catch (error) {
+        console.log(error);
+        throw error;
     }
-
-    const analysis = await response.json();
-
-    return analysis;
 
 };
 
