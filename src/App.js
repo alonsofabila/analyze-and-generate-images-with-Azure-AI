4d916082ee4d99b68bd518583de2b12c50e85e9f
alonso fabila <jorgealonsofabila@gmail.com>
2023-11-03 11:20:44 -0600

    Added a UI for the page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <div class="container">
+      <h1>Computer Vision</h1>
+      <label>
+        <input type='text' placeholder='Enter URL to analyze or textual prompt to generate an image'></input>
+      </label>
+      <button>Analyze</button>
+      <button>Generate</button>
+    </div>
+  );
 }
 
 export default App;
